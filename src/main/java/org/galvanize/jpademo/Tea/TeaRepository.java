434c864d5475908dffc587e09cb5ef8cf7b5a01b
K Hudson <kay.hudson@galvanize.com>
2021-01-09 07:53:58 -0800

    demo

--- src/main/java/org/galvanize/jpademo/Tea/TeaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.galvanize.jpademo.Tea;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeaRepository extends JpaRepository<Tea, String> {
-}
